2117121120c1c3894984bc5e3f649ef548fede17
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-01 02:10:50 -0600

    Keep user session using localStorage and emulate when user logs out

--- src/auth/context/AuthContext.tsx
+++ src/auth/context/AuthContext.tsx
@@ -4,6 +4,7 @@ import { AuthState } from './authReducer';
 export interface AuthContextProps {
   authState: AuthState;
   login: (id: string, name: string) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext({} as AuthContextProps);
--- src/auth/context/AuthProvider.tsx
+++ src/auth/context/AuthProvider.tsx
@@ -4,21 +4,36 @@ import { AuthContext } from './AuthContext';
 import { AuthAction, authReducer } from './authReducer';
 import { types } from '../types';
 
-const initialState = {
-  loggedIn: false,
-  user: undefined,
+const initializer = () => {
+  const user = JSON.parse(String(localStorage.getItem('user'))) || undefined;
+
+  return {
+    loggedIn: !!user,
+    user,
+  };
 };
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, initializer);
 
   const login = (id: string, name: string) => {
+    const user = { id, name };
     const action: AuthAction = {
       type: types.login,
-      payload: {
-        id,
-        name,
-      },
+      payload: user,
+    };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    dispatch(action);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+
+    const action: AuthAction = {
+      type: types.logout,
+      payload: undefined,
     };
 
     dispatch(action);
@@ -29,6 +44,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       value={{
         authState,
         login,
+        logout,
       }}
     >
       {children}
